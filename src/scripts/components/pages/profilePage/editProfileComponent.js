987a9edd57c722f28f21225ad002e62cf6926c8d
james sewell <jamesjosephsewell@gmail.com>
2017-07-13 09:52:23 -0500

    specified all buttons by the button type on profile edit page

--- src/scripts/components/pages/profilePage/editProfileComponent.js
+++ src/scripts/components/pages/profilePage/editProfileComponent.js
@@ -407,6 +407,7 @@ class EditProfile extends Component {
 
                                     <Modal.Actions>
                                         <Button
+                                            type="button"
                                             content="no"
                                             onClick={() => {
                                                 this.setState({
@@ -415,6 +416,7 @@ class EditProfile extends Component {
                                             }}
                                         />
                                         <Button
+                                            type="button"
                                             icon="check"
                                             content="remove photo"
                                             onClick={this.removeProfileImage.bind(
